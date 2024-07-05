8f6f1473eff08217b676346c2b7bf6dcde0f6124
Ayush Nayak <nayaksubham18@gmail.com>
2024-07-05 11:29:57 +0530

    init

---
+++ src/config/database.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+
+mongoose.connect(config.mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', ()=> {
+    console.log('MongoDB connection error:', error)
+});
+
+db.once('open', () => {
+    console.log('Connected to MongoDB');
+});
+
+module.exports = db;
