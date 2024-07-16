d34ff45b31710055bd7a4dce6e4647700b125ef6
Ayush Nayak <51288222+Ayush1832@users.noreply.github.com>
2024-07-16 11:11:06 +0530

    Delete src directory

--- src/config/config.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    port: process.env.PORT || 3000,
-    jwtSecret: process.env.JWT_SECRET,
-    emailUser: process.env.EMAIL_USER,
-    emailPassword: process.env.EMAIL_PASSWORD,
-    mongoUri: process.env.MONGO_URI,
-};
