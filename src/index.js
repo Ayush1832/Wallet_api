8e0dab825fcc7429b32a33f941cc7eff81fa9c87
Ayush Nayak <51288222+Ayush1832@users.noreply.github.com>
2024-07-18 01:31:03 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,36 +1,20 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-import userRoutes from './routes/userRoutes.js'; // Add .js extension
-import transactionRoutes from './routes/transactionRoutes.js'; // Add .js extension
+import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
+import transactionRoutes from './routes/transactionRoutes.js';
 
 dotenv.config();
 
+connectDB();
+
 const app = express();
 
-// Middleware
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Routes
 app.use('/api/users', userRoutes);
 app.use('/api/transactions', transactionRoutes);
 
-
-const mongoUrl = 'mongodb://127.0.0.1:27017/wallet';
-
-if (!mongoUrl) {
-    throw new Error('MONGODB_URI is not defined in the environment variables');
-  }
-
-  mongoose.connect(mongoUrl).then(() => {
-    console.log('Connected to MongoDB');
-  }).catch((err) => {
-    console.error('Error connecting to MongoDB:', err);
-  });
-
-// Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
